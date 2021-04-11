91c4f4409f438a35a472784cc6b5a89b57849157
Jiyoung Jenna Yeo <ysy91231@gmail.com>
2021-04-11 14:43:29 +0900

    버킷리스트 아이템 추가하기

--- BucketList/src/App.js
+++ BucketList/src/App.js
@@ -1,5 +1,7 @@
+//순서
+// 뷰 만들기 -> ref설정해서 텍스트 가져오기(input박스에서 입력한 것은 current.value) -> 
+
 import React from "react";
-import logo from "./logo.svg";
 // BucketList 컴포넌트를 import 해옵니다.
 // import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
 import BucketList from "./BucketList";
@@ -13,6 +15,24 @@ class App extends React.Component {
     this.state = {
       list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
     };
+
+    // 밑에서 뷰 만든 후 여기로
+    this.text = React.createRef();
+    // 이후엔 밑에 return에서 불러줌(ref)
+  }
+
+  // 함수 생성
+  addBucketList = () => {
+    let list = this.state.list;
+    const new_item = this.text.current.value;
+
+    this.setState({list: [...list, new_item]})
+    // 스프레드문법: 배열의 리스트 하나하나하나가 들어감 그리고 new_item이 배열에 새로 추가됨
+    // this.state값은 변하지않고 배열이 새로 생성되는것임 -> 불변성 유지
+  }
+
+  componentDidMount(){
+    console.log(this.text);
   }
 
   // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
@@ -26,11 +46,29 @@ class App extends React.Component {
           {/* <컴포넌트 명 [props 명]={넘겨줄 것(리스트, 문자열, 숫자, ...)}/> */}
           <BucketList list={this.state.list} />
         </Container>
+        <div>
+          <Input>
+            <input type="text" ref={this.text}/>
+            {/* input에 ref연결 시킨 후 위로 가서 함수 생성 */}
+            <button onClick={this.addBucketList}>추가하기</button>
+            {/* 함수 생성 후 버튼에 연결 */}
+          </Input>
+        </div>
+
       </div>
     );
   }
 }
 
+const Input = styled.div`
+  max-width: 350px;
+  min-height: 10vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
 const Container = styled.div`
   max-width: 350px;
   min-height: 80vh;
